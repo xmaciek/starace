6f012a769e31b14180ffa8401c9a49f86a5d767e
Maciej Latocha <latocha.maciek@gmail.com>
2016-05-27 23:43:14 +0200

    Update Button class

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,91 +1,95 @@
 #include "Button.h"
 
+#include "shader.hpp"
 
-
-bool Button::IsClicked(GLuint X, GLuint Y) {
-  if (!enabled) { return false; }
-  if ((X>=x) && (X<=x+width) && (Y>=y) && (Y<=y+height)) {
-    return true;
-  }
-  else { return false; }
+bool Button::IsClicked( uint32_t x, uint32_t y ) {
+    if ( !m_enabled ) {
+        return false;
+    }
+    if ( ( x >= m_x ) && ( x <= m_x + m_width ) && ( y >= m_y ) && ( y <= m_y + m_height ) ) {
+        return true;
+    }
+    return false;
 }
 
 
 void Button::Draw() {
-  glEnable(GL_BLEND);
-  glEnable(GL_TEXTURE_2D);
-  glPushMatrix();
-    glTranslated(x,y,0);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    if (enabled) { glColor3f(0,0.75,1); }
-      else { glColor3f(0.3, 0.55, 0.65); }
-    glBegin(GL_QUADS);
-      glTexCoord2d(0,0); glVertex2d(0, height);
-      glTexCoord2d(0,1); glVertex2d(0, 0);
-      glTexCoord2d(1,1); glVertex2d(width, 0);
-      glTexCoord2d(1,0); glVertex2d(width, height);
-    glEnd();
-
+    SHADER::pushMatrix();
+    SHADER::translate( m_x, m_y, 0 );
+    if ( m_enabled ) {
+        SHADER::setColor( 0, 0.75, 1, 1 );
+    } else {
+        SHADER::setColor( 0.3, 0.55, 0.65, 1 );
+    }
+    // TODO: reenable texture
+//     glBindTexture(GL_TEXTURE_2D, textureID);
+    if ( !m_bufferID ) {
+        m_bufferID = SHADER::getQuad( 0, 0, m_width, m_height );
+    }
+    SHADER::draw( GL_TRIANGLES, m_bufferID, 6 );
+    // TODO: reenable font rendering
+#if 0
     if (font!=NULL) { 
       glColor4f(1.0f,1.0f,1.0f,1.0f);
       glTranslated(width/2-text_length, 0, 0);
       font->PrintTekst(0, height/2-font->GetMiddlePoint(), text.c_str()); 
     }
-  glPopMatrix();
-  glDisable(GL_TEXTURE_2D);
-  glDisable(GL_BLEND);
-  
+#endif
+    SHADER::popMatrix();
+
 }
 
-void Button::MouseOver(GLuint X, GLuint Y) {};
+void Button::MouseOver( uint32_t x, uint32_t y ) {};
 
-void Button::UpdateCoord(GLuint X, GLuint Y) {
-  x = X;
-  y = Y;
+void Button::UpdateCoord( uint32_t x, uint32_t y ) {
+    m_x = x;
+    m_y = y;
 };
 
-void Button::SetTexture(GLuint t) { textureID = t; }
-
-Button::Button(Font* F, GLuint X, GLuint Y, GLuint W, GLuint H) {
-  x = X;
-  y = Y;
-  width = W;
-  height = H;
-  font = F;
-  enabled = true;
-  textureID = 0;
+void Button::SetTexture( uint32_t t ) {
+    m_textureID = t;
 }
 
-Button::Button() {
-  textureID = 0;
-  x = y = 0;
-  width = 192;
-  height = 48;
-  enabled = true;
-  font = NULL;
-  text_length = 0;
+Button::Button( Font* f, uint32_t x, uint32_t y, uint32_t w, uint32_t h ) {
+    m_x = x;
+    m_y = y;
+    m_width = w;
+    m_height = h;
+    m_font = f;
+    m_enabled = true;
+    m_textureID = 0;
+    m_bufferID = 0;
+    m_text_length = 0;
 }
 
-void Button::SetSize(const GLuint &W, const GLuint &H) {
-  width = W;
-  height = H;
-}
-
-void Button::SetFont(Font *F) {
-  font = F;
-  if (font!=NULL) { text_length = font->GetTextLength(text.c_str())/2; }
-}
 Button::~Button() {};
 
-void Button::Enable(const bool &B) { enabled = B; }
-void Button::Enable() { enabled = true; }
-void Button::Disable() { enabled = false; }
+void Button::SetSize( uint32_t w, uint32_t h ) {
+    m_width = w;
+    m_height = h;
+    SHADER::deleteBuffer( m_bufferID );
+    m_bufferID = SHADER::getQuad( 0, 0, m_width, m_height );
+}
 
-bool Button::IsEnabled() { return enabled; }
+void Button::SetFont( Font *F ) {
+    m_font = F;
+    if ( m_font ) {
+        m_text_length = m_font->GetTextLength( text.c_str() ) / 2;
+    }
+}
+
+void Button::Enable( bool b ) { m_enabled = b; }
+void Button::Enable() { m_enabled = true; }
+void Button::Disable() { m_enabled = false; }
+
+bool Button::IsEnabled() { return m_enabled; }
 
 void Button::SetText(const char* txt) {
-  text = txt;
-  if (font!=NULL) { text_length = font->GetTextLength(text.c_str())/2; }
-    else { text_length = 0; }
+    text = txt;
+    if ( m_font ) {
+        m_text_length = m_font->GetTextLength( text.c_str() ) / 2;
+    } else {
+        m_text_length = 0;
+    }
 }
 
--- src/Button.h
+++ src/Button.h
@@ -1,39 +1,32 @@
-#ifndef SA_BUTTON
-#define SA_BUTTON
+#pragma once
 
 #include "SA.h"
 #include "Font.h"
 
 class Button {
 private:
-  GLuint x, y, width, height, textureID; 
-  Font* font;
-  GLfloat color[3];
-  bool enabled;
-  GLuint text_length;
-  
+    uint32_t m_x, m_y, m_width, m_height;
+    uint32_t m_textureID;
+    uint32_t m_bufferID;
+    Font* m_font;
+    float m_color[3];
+    bool m_enabled;
+    uint32_t m_text_length;
+
 public:
-  Button();
-  Button(Font* F, GLuint X, GLuint Y, GLuint W, GLuint H);
-  ~Button();
-  std::string text;
-  void SetTexture(GLuint t);
-  bool IsClicked(GLuint X, GLuint Y);
-  void MouseOver(GLuint X, GLuint Y);
-  void Draw();
-  void UpdateCoord(GLuint X, GLuint Y);
-  void SetFont(Font *F);
-  void SetSize(const GLuint &W, const GLuint &H);
-  void Enable(const bool &B);
-  void Enable();
-  void Disable();
-  bool IsEnabled();
-  void SetText(const char *txt);
+    Button( Font* f = 0, uint32_t x = 0, uint32_t y = 0, uint32_t w = 192, uint32_t h = 48 );
+    ~Button();
+    std::string text;
+    void SetTexture( uint32_t t );
+    bool IsClicked( uint32_t x, uint32_t y );
+    void MouseOver( uint32_t x, uint32_t y );
+    void Draw();
+    void UpdateCoord( uint32_t x, uint32_t y );
+    void SetFont( Font *F);
+    void SetSize( uint32_t w, uint32_t h );
+    void Enable( bool b );
+    void Enable();
+    void Disable();
+    bool IsEnabled();
+    void SetText( const char *txt );
 };
-
-
-
-
-
-
-#endif
