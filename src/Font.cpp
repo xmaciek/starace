bdf0c9c4a2785cf747a133733f9148ef385745cd
Maciej Latocha <latocha.maciek@gmail.com>
2016-05-28 10:38:25 +0200

    Update Font class
    Also added struct Glyph to store information about given character.
    Class Font now uses map of these glyphs instead of display lists.

--- src/Font.cpp
+++ src/Font.cpp
@@ -1,113 +1,110 @@
 #include "Font.h"
-Font::Font(const char* fontname, GLuint h) {
-  std::cout<<"+-- Creating Font "<<fontname<<":"<<h<<"\n";
-  name = fontname;
-  textures = new GLuint[128];
-  char_length = new GLuint[128];
-  height = h;
-//   middlepoint = h/2;
 
-  list_base = glGenLists(128);
-  glGenTextures(128, textures);
-  TTF_Font *font = TTF_OpenFont(fontname, h);
-  
-  for (GLubyte i=0; i<128; i++) {
-    make_dlist(font, i);
-  }
-  TTF_CloseFont(font);
-  
+Glyph::Glyph() :
+    m_textureID( 0 ),
+    m_bufferID( 0 ),
+    m_textureCoordID( 0 ),
+    m_width( 0 )
+{}
 
+Font::Font( const std::string& fontName, uint32_t h ) :
+    m_name( fontName ),
+    m_height( h ),
+    m_middlePoint( h / 2 )
+{
+    std::cout << "+-- Creating Font " << fontName << ":" << h << std::endl;
+
+    TTF_Font *font = TTF_OpenFont( fontName.c_str(), h );
+    for ( uint32_t i = 0; i < 128; i++ ) {
+        createGlyphFor( i, font );
+    }
+    TTF_CloseFont( font );
 }
 
-Font::~Font() {
-  std::cout<<"+-- Deleting Font "<<name<<":"<<height<<"\n";
-  glDeleteLists(list_base, 128);
-  glDeleteTextures(128, textures);
-  delete[] textures;
+Font::~Font() {}
+
+static uint32_t pow2( uint32_t a ) {
+    uint32_t r = 4;
+    while ( r < a ) { r *= 2; }
+    return r;
 }
 
+void Font::createGlyphFor( uint32_t characterValue, TTF_Font* font ) {
+    Glyph glyph;
+    glGenTextures( 1, &glyph.m_textureID );
+    glyph.m_textureCoordID = SHADER::getQuadTextureCoord( 0, 1, 1, 0 );
+    {
+        SDL_Color color = { 255, 255, 255 };
+        SDL_Surface* tmp = TTF_RenderGlyph_Blended( font, characterValue, color );
 
-GLuint Font::pow2(GLint a) {
-  GLuint r = 4;
-  while (r<(GLuint)a) { r*=2; }
-  return r;
-}
+        int32_t minX, maxX, minY, maxY;
+        TTF_GlyphMetrics( font, characterValue, &minX, &maxX, &minY, &maxY, &glyph.m_width );
 
-void Font::make_dlist (TTF_Font *font, GLuint ch) {
-  
-  SDL_Color col = {255,255,255};
-  SDL_Surface *tmp = NULL;
+        uint32_t optW = pow2( tmp->w );
+        uint32_t optH = pow2( m_height );
+        glyph.m_bufferID = SHADER::getQuad( 0, 0, optW, optH );
 
-  tmp = TTF_RenderGlyph_Blended(font, (Uint16)ch, col);
- 
-  GLint minX,maxX,minY,maxY,advance;
-  TTF_GlyphMetrics(font, ch, &minX, &maxX, &minY, &maxY, &advance);
+        SDL_Rect rect;
+        rect.x = minX;
+        rect.y = m_height - maxY;
 
-  GLuint optW=pow2(tmp->w), optH=pow2(height);
-  SDL_Rect rect;
-  rect.x =  minX;
-  rect.y =  height-maxY;  
-  
-  SDL_Surface* expanded_data = SDL_CreateRGBSurface(tmp->flags, optW, optH, 32, tmp->format->Amask, tmp->format->Gmask, tmp->format->Bmask, tmp->format->Rmask);
-  SDL_BlitSurface(tmp, NULL, expanded_data, &rect);
-  SDL_FreeSurface(tmp);
-  
-  GLuint index=0;
-  GLubyte pixels[optH*optW*2];
-  GLubyte *pix = (GLubyte*)expanded_data->pixels;
-  for (GLuint i=0; i<(optH)*(optW)*4; i+=4) {
-    pixels[index] = pix[i];
-    pixels[index+1] = pix[i];
-    index+=2;
-  }
-  
-  glEnable(GL_TEXTURE_2D);
-  glBindTexture(GL_TEXTURE_2D, textures[ch]);
-  setTextureFiltering(-1);
-  gluBuild2DMipmaps( GL_TEXTURE_2D, 2, optW, optH, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, pixels);
-  SDL_FreeSurface(expanded_data);
-  char_length[ch] = advance;
-  middlepoint = optH/2;
-  glNewList(list_base+ch, GL_COMPILE);
-    glBindTexture(GL_TEXTURE_2D, textures[ch]); 
-    glPushMatrix();
-      glBegin(GL_QUADS);
-        glTexCoord2d(0, 0); glVertex2d(0, optH);
-        glTexCoord2d(0, 1); glVertex2d(0, 0);
-        glTexCoord2d(1, 1); glVertex2d(optW, 0);
-        glTexCoord2d(1, 0); glVertex2d(optW, optH);
-      glEnd();
-    glPopMatrix();
-    glTranslated(advance, 0, 0);
-  glEndList();
-//   glDisable(GL_TEXTURE_2D);
-}
+        SDL_Surface* expanded_data = SDL_CreateRGBSurface( tmp->flags, optW, optH, 32,
+            tmp->format->Amask, tmp->format->Gmask, tmp->format->Bmask, tmp->format->Rmask );
+        SDL_BlitSurface( tmp, NULL, expanded_data, &rect );
+        SDL_FreeSurface( tmp );
 
-GLuint Font::GetTextLength(const char* tekst) {
-  string txt = tekst;
-  GLuint length = 0;
-  for (GLuint i=0; i<txt.size(); i++) {
-    length+=char_length[txt[i]];
-  }
-  return length;
+        //for some reason, SDL creates yellow glyph textures, I want them white
+        uint8_t* pixPtr = (uint8_t*)expanded_data->pixels;
+        for ( uint32_t i = 0; i < optH * optW * 4; i += 4 ) {
+            pixPtr[i] = pixPtr[i];
+            pixPtr[i + 1] = pixPtr[i];
+            pixPtr[i + 2] = pixPtr[i];
+        }
+
+        glBindTexture( GL_TEXTURE_2D, glyph.m_textureID );
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, optW, optH, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixPtr );
+        glGenerateMipmap( GL_TEXTURE_2D );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16.0f );
+        SDL_FreeSurface( expanded_data );
+
+        m_middlePoint = optH/2;
+    }
+    m_glyphMap.insert( std::make_pair( characterValue, glyph ) );
 }
 
 
-void Font::PrintTekst(const GLdouble &x, const GLdouble &y, const char* tekst) {
-  stringtxt = tekst;
-  glListBase(list_base);
-  glPushMatrix();
-    glTranslated(x,y,0);
-    glEnable(GL_BLEND);
-    glEnable(GL_TEXTURE_2D);
-//     glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-    glCallLists(stringtxt.length(), GL_UNSIGNED_BYTE, stringtxt.c_str());
-//     glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-    glDisable(GL_TEXTURE_2D);
-    glDisable(GL_BLEND);
-  glPopMatrix();
+uint32_t Font::GetTextLength( const std::string& text ) const {
+    uint32_t length = 0;
+    std::string::const_iterator it = text.begin();
+    while ( it != text.end() ) {
+        GlyphMap::const_iterator git = m_glyphMap.find( *it );
+        if ( git != m_glyphMap.end() ) {
+            length += git->second.m_width;
+        }
+        it++;
+    }
+    return length;
+}
+
+void Font::PrintTekst( double x, double y, const std::string& text ) const {
+    SHADER::translate( x, y, 0 );
+    std::string::const_iterator it = text.begin();
+    while ( it != text.end() ) {
+        GlyphMap::const_iterator git = m_glyphMap.find( *it );
+        if ( git != m_glyphMap.end() ) {
+            glBindTexture( GL_TEXTURE_2D, git->second.m_textureID );
+            SHADER::setTextureCoord( git->second.m_textureCoordID );
+            SHADER::draw( GL_TRIANGLES, git->second.m_bufferID, 6 );
+            SHADER::translate( git->second.m_width, 0, 0 );
+        }
+        it++;
+    }
 }
 
 
-GLuint Font::GetHeight() { return height; }
-GLuint Font::GetMiddlePoint() { return middlepoint; }
+uint32_t Font::GetHeight() const { return m_height; }
+uint32_t Font::GetMiddlePoint() const { return m_middlePoint; }
--- src/Font.h
+++ src/Font.h
@@ -1,27 +1,33 @@
-#ifndef SA_FONT
-#define SA_FONT
+#pragma once
 
 #include "SA.h"
 #include "Texture.h"
 
-class Font {
-private:
-  string stringtxt;
-  string name;
-  GLuint height, middlepoint;
-  GLuint *textures;
-  GLuint *char_length;
-  GLuint list_base;
-  void make_dlist(TTF_Font *font, GLuint ch);
-  GLuint pow2(GLint a);
-public:
-  Font(const char* fontname, GLuint h);
-  ~Font();
-  GLuint GetHeight();
-  GLuint GetMiddlePoint();
-  void PrintTekst(const GLdouble &x, const GLdouble &y, const char* tekst);
-  GLuint GetTextLength(const char* tekst);
-  GLuint GetTextLength(const string &tekst);
+#include <map>
+
+struct Glyph {
+    Glyph();
+    uint32_t m_textureID;
+    uint32_t m_bufferID;
+    uint32_t m_textureCoordID;
+    int32_t m_width;
 };
 
-#endif
+class Font {
+private:
+    std::string m_name;
+    uint32_t m_height, m_middlePoint;
+
+    // <character value, Glyph>
+    typedef std::map<uint32_t, Glyph> GlyphMap;
+    GlyphMap m_glyphMap;
+    void createGlyphFor( uint32_t characterValue, TTF_Font* font );
+
+public:
+    Font( const std::string& fontName, uint32_t h );
+    ~Font();
+    uint32_t GetHeight() const ;
+    uint32_t GetMiddlePoint() const ;
+    void PrintTekst( double x, double y, const std::string& text ) const;
+    uint32_t GetTextLength( const std::string& text ) const;
+};
