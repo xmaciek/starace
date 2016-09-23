081ca21d457c66b270e9c3546cd0c70c1a78b3ab
Maciej Latocha <latocha.maciek@gmail.com>
2016-09-23 20:39:19 +0200

    Rework model class
    Load .obj file correctly.

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,6 +1,9 @@
 #include "Model.h"
 #include "SA.h"
 
+#include <algorithm>
+#include <map>
+
 using namespace std;
 
 Model::Model() :
@@ -14,9 +17,15 @@ Model::~Model()
 
 
 void Model::Draw() {
+    if ( !m_bufferID || !m_bufferID || !m_uvID ) {
+        return;
+    }
     SHADER::pushMatrix();
+    glEnable( GL_DEPTH_TEST );
     SHADER::setTextureCoord( m_uvID );
+    m_texture.use();
     SHADER::draw( GL_TRIANGLES, m_bufferID, m_vertices );
+    glDisable( GL_DEPTH_TEST );
     SHADER::popMatrix();
 }
 
@@ -28,10 +37,11 @@ static void lineToVertices( const std::string& line, std::vector<double>& vertic
     vertices.push_back( z );
 }
 
-static void lineToVec3( const std::string& line, std::vector<glm::vec3>& vector ) {
+// no assign operator to glm::vec3 ?
+static void lineToVec3( const std::string& line, std::vector<glm::vec3> &vec ) {
     glm::vec3 v;
     sscanf( line.c_str(), "v %f %f %f ", &v[0], &v[1], &v[2] );
-    vector.push_back( v );
+    vec.push_back( v );
 }
 
 
@@ -39,79 +49,142 @@ static uint32_t makeMappedBuffer( const std::vector<double>& v, const std::vecto
     if ( v.empty() || m.empty() ) {
         return 0;
     }
-    std::vector<double> array( m.size() * 3 );
-    uint32_t j = 0;
-    for ( uint32_t i = 0; i < m.size(); i++ ) {
-        array[j++] = v[ m[i] * 3 + 0 ];
-        array[j++] = v[ m[i] * 3 + 1 ];
-        array[j++] = v[ m[i] * 3 + 2 ];
+    std::vector<double> array;
+    for ( std::vector<uint32_t>::const_iterator it = m.begin(); it != m.end(); ++it ) {
+        array.push_back( v.at( ( *it - 1 ) * 3 + 0 ) );
+        array.push_back( v.at( ( *it - 1 ) * 3 + 1 ) );
+        array.push_back( v.at( ( *it - 1 ) * 3 + 2 ) );
     }
-    return SHADER::makeBuffer( &array[0], array.size() );
+    return SHADER::makeBuffer( array );
 }
 
 static uint32_t makeMappedUVBuffer( const std::vector<double>& uv, const std::vector<uint32_t>& m ) {
     if ( uv.empty() || m.empty() ) {
         return 0;
     }
-    std::vector<double> array( m.size() * 2 );
-    uint32_t j = 0;
-    for ( uint32_t i = 0; i < m.size(); i++ ) {
-        array[j++] = uv[ m[i] * 2 + 0 ];
-        array[j++] = uv[ m[i] * 2 + 1 ];
+    std::vector<double> array;
+    for ( std::vector<uint32_t>::const_iterator it = m.begin(); it != m.end(); ++it ) {
+        array.push_back( uv.at( ( *it - 1 ) * 2 + 0 ) );
+        array.push_back( uv.at( ( *it - 1 ) * 2 + 1 ) );
     }
-    return SHADER::makeBuffer( &array[0], array.size() );
+    return SHADER::makeBuffer( array );
+}
+
+struct FileSection { enum Enum { Unknown, Hull, Weapons, Thrusters }; };
+struct DataSection { enum Enum { Unknown, Ignore, Object, Vertex, UV, Face, FaceTexture, FaceTextureNormal }; };
+
+static FileSection::Enum guessFileSection( const std::string& line )
+{
+    static std::map<std::string, FileSection::Enum> map;
+    if ( map.empty() ) {
+        map[ "hull" ] = FileSection::Hull;
+        map[ "weapons" ] = FileSection::Weapons;
+        map[ "thruster" ] = FileSection::Thrusters;
+    }
+    std::map<std::string, FileSection::Enum>::const_iterator it = map.find( line.substr( 2 ) );
+    return ( it != map.end() ) ? it->second : FileSection::Unknown;
+}
+
+static DataSection::Enum guessDataSection( const std::string& line )
+{
+    assert( !line.empty() );
+    switch ( line[0] ) {
+        case '#':
+        case 's':
+        case 'u':
+        case 'm': return DataSection::Ignore;
+        case 'o': return DataSection::Object;
+        case 'v':
+            assert( line.size() > 2 );
+            switch ( line[1] ) {
+                case ' ': return DataSection::Vertex;
+                case 't': return DataSection::UV;
+                case 'n': return DataSection::Ignore; // ignore normals for now
+                default: return DataSection::Unknown;
+            }
+        case 'f':
+            switch ( std::count( line.begin(), line.end(), '/' ) ) {
+                case 0: return DataSection::Face;
+                case 3: return DataSection::FaceTexture;
+                case 6: return DataSection::FaceTextureNormal;
+                default: return DataSection::Ignore;
+            }
+        default: return DataSection::Unknown;
+    }
 }
 
 void Model::Load_OBJ( const std::string& fileName ) {
-    fprintf( stdout, "Loading model: %s", fileName.c_str() );
-
-    std::fstream modelFile( fileName, std::fstream::in );
     std::string line;
     std::vector<double> vertices;
     std::vector<uint32_t> verticesMap;
     std::vector<double> uv;
     std::vector<uint32_t> uvMap;
-    uint8_t verticesDataMode = 0;
-    bool modelContainsTexture = false;
 
+    FileSection::Enum fileSection = FileSection::Unknown;
+
+    std::fstream modelFile( fileName, std::fstream::in );
     while ( getline( modelFile, line ) ) {
-        const std::string subStr( line.substr( 0, 2 ) );
-        if ( subStr == "v " ) {
-            switch ( verticesDataMode ) {
-                case 1: lineToVertices( line, vertices ); break;
-                case 2: lineToVec3( line, m_weaponBanks ); break;
-                case 3: lineToVec3( line, m_thrusterBanks ); break;
-                default: break;
-            }
-        } else if ( subStr == "o " ) {
-            line.erase( 0, 2 );
-            if ( line == "hull" ) { verticesDataMode = 1; }
-            else if ( line == "weapons" ) { verticesDataMode = 2; }
-            else if ( line == "thruster") { verticesDataMode = 3; }
-            else { verticesDataMode = 0; }
-        } else if ( subStr == "vt" ) {
-            modelContainsTexture = true;
-            double u, v;
-            sscanf( line.c_str(), "vt %lf %lf ", &u, &v );
-            uv.push_back( u );
-            uv.push_back( v );
-        } else if ( subStr == "f " ) {
-            uint32_t v1, v2, v3;
-            if ( modelContainsTexture ) {
+        switch ( guessDataSection( line ) ) {
+            case DataSection::Object:
+                fileSection = guessFileSection( line );
+                break;
+            case DataSection::Vertex:
+                switch ( fileSection ) {
+                    case FileSection::Hull:
+                        lineToVertices( line, vertices );
+                        break;
+                    case FileSection::Weapons:
+                        lineToVec3( line, m_weaponBanks );
+                        lineToVertices( line, vertices );
+                        break;
+                    case FileSection::Thrusters:
+                        lineToVec3( line, m_thrusterBanks );
+                        lineToVertices( line, vertices );
+                        break;
+                    default:
+                        assert( !"Wrong file section in vertex parse" );
+                        break;
+                } break;
+                case DataSection::UV: {
+                    double u, v;
+                    sscanf( line.c_str(), "vt %lf %lf", &u, &v );
+                    uv.push_back( u );
+                    uv.push_back( v );
+                } break;
+            case DataSection::Face: {
+                uint32_t v1, v2, v3;
+                sscanf( line.c_str(), "f %i %i %i", &v1, &v2, &v3 );
+                verticesMap.push_back( v1 );
+                verticesMap.push_back( v2 );
+                verticesMap.push_back( v3 );
+            } break;
+            case DataSection::FaceTexture: {
+                uint32_t v1, v2, v3;
                 uint32_t t1, t2, t3;
                 sscanf( line.c_str(), "f %i/%i %i/%i %i/%i", &v1, &t1, &v2, &t2, &v3, &t3 );
                 uvMap.push_back( t1 );
                 uvMap.push_back( t2 );
                 uvMap.push_back( t3 );
-            } else {
-                sscanf( line.c_str(), "f %i %i %i", &v1, &v2, &v3 );
-            }
-            verticesMap.push_back( v1 );
-            verticesMap.push_back( v2 );
-            verticesMap.push_back( v3 );
+                verticesMap.push_back( v1 );
+                verticesMap.push_back( v2 );
+                verticesMap.push_back( v3 );
+            } break;
+            case DataSection::FaceTextureNormal: {
+                uint32_t v1, v2, v3;
+                uint32_t t1, t2, t3;
+                uint32_t n1, n2, n3;
+                sscanf( line.c_str(), "f %i/%i/%i %i/%i/%i %i/%i/%i", &v1, &t1, &n1, &v2, &t2, &n2, &v3, &t3, &n3 );
+                uvMap.push_back( t1 );
+                uvMap.push_back( t2 );
+                uvMap.push_back( t3 );
+                verticesMap.push_back( v1 );
+                verticesMap.push_back( v2 );
+                verticesMap.push_back( v3 );
+            } break;
+            case DataSection::Ignore: break;
+            default: assert( !"Unknown data section" ); break;
         }
     }
-
     modelFile.close();
 
     m_vertices = verticesMap.size();
@@ -149,8 +222,6 @@ void Model::Load_OBJ( const std::string& fileName ) {
     cout <<"done.\n";
 #endif
   }
-
-void Model::BindTexture( uint32_t ) {}
 
   void Model::Scale(GLfloat scale) {
 #if 0
--- src/Model.h
+++ src/Model.h
@@ -10,12 +10,13 @@ private:
     uint32_t m_bufferID;
     uint32_t m_uvID;
     std::vector<glm::vec3> m_weaponBanks, m_thrusterBanks;
+    Texture m_texture;
 
 public:
     Model();
     ~Model();
 
-    void BindTexture( uint32_t );
+    inline void setTexture( const std::string& fileName ) { m_texture.load( fileName ); }
     void Draw();
     void Load_OBJ( const std::string& fileName );
     void CalculateNormal();
