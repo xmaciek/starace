9734204d1df7020d0c1c337b3d51d5b573b24feb
Maciej Latocha <latocha.maciek@gmail.com>
2020-09-19 14:58:11 +0200

    remove srand

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char** argv )
 {
     std::cout << "Launching...\n";
-    srand( (unsigned)time( NULL ) );
     Road* road = new Road();
     int ret = road->OnExecute();
     delete road;
