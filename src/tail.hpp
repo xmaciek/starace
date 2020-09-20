2f36f2358e77914ad40f4e4f3a992e851ddd78c0
Maciej Latocha <latocha.maciek@gmail.com>
2020-09-20 16:53:37 +0200

    clang-tidy tail naming

--- src/tail.hpp
+++ src/tail.hpp
@@ -6,14 +6,14 @@
 #include <deque>
 
 class Tail {
+private:
+    std::deque<Vertex> m_segments;
+
 public:
     Tail() = default;
     Tail( uint16_t segments, const Vertex& v );
-    typedef std::deque<Vertex>::const_iterator const_iterator;
+    using const_iterator  = std::deque<Vertex>::const_iterator;
     const_iterator begin() const;
     const_iterator end() const;
     void insert( const Vertex& v );
-
-private:
-    std::deque<Vertex> m_segments;
 };
