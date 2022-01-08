0ba49d0ebeb20537ffb040b0ddeec39fc88c08e0
Maciej Latocha <latocha.maciek@gmail.com>
2022-01-08 18:07:17 +0100

    add bit index iterator

---
+++ src/shared/public/shared/bit_index_iterator.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <bit>
+#include <cstdint>
+
+struct BitIndexIterator
+{
+    using value_type = uint64_t;
+    value_type m_integer = 0;
+
+    constexpr BitIndexIterator() noexcept = default;
+    constexpr BitIndexIterator( value_type t ) noexcept
+    : m_integer{ t }
+    {}
+
+    constexpr BitIndexIterator& operator = ( value_type t ) noexcept
+    {
+        m_integer = t;
+        return *this;
+    }
+
+    constexpr uint32_t operator * () const noexcept
+    {
+        return std::countr_zero( m_integer );
+    }
+
+    constexpr operator bool () const noexcept
+    {
+        return m_integer != 0;
+    }
+
+    constexpr BitIndexIterator& operator ++ () noexcept
+    {
+        const int idx = **this;
+        const value_type tint = 1ull << idx;
+        m_integer &= ~tint;
+        return *this;
+    }
+
+    constexpr BitIndexIterator operator ++ ( int ) noexcept
+    {
+        BitIndexIterator ret = *this;
+        ++*this;
+        return ret;
+    }
+
+    constexpr uint32_t count() const noexcept
+    {
+        return std::popcount( m_integer );
+    }
+};
