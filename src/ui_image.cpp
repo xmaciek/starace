6ce8c81c305bf7c587dcad40927629e118435743
Maciej Latocha <latocha.maciek@gmail.com>
2022-04-17 23:51:46 +0200

    allow ui::Image to use data models

--- src/ui_image.cpp
+++ src/ui_image.cpp
@@ -1,25 +1,28 @@
 #include "ui_image.hpp"
 
 #include "game_pipeline.hpp"
+#include "ui_data_model.hpp"
 
 #include <renderer/renderer.hpp>
 
 #include <cassert>
 
-UIImage::UIImage( Texture t )
+namespace ui {
+
+Image::Image( Texture t )
 : m_texture{ t }
 {
     assert( t );
 };
 
-UIImage::UIImage( Texture t, math::vec4 color )
+Image::Image( Texture t, math::vec4 color )
 : m_color{ color }
 , m_texture{ t }
 {
     assert( t );
 };
 
-UIImage::UIImage( math::vec2 position, math::vec2 extent, math::vec4 color, Texture t )
+Image::Image( math::vec2 position, math::vec2 extent, math::vec4 color, Texture t )
 : Widget{ position, extent }
 , m_color{ color }
 , m_texture{ t }
@@ -27,7 +30,7 @@ UIImage::UIImage( math::vec2 position, math::vec2 extent, math::vec4 color, Text
     assert( t );
 }
 
-UIImage::UIImage( Anchor a, math::vec2 extent, math::vec4 color, Texture t )
+Image::Image( Anchor a, math::vec2 extent, math::vec4 color, Texture t )
 : Widget{ {}, extent, a }
 , m_color{ color }
 , m_texture{ t }
@@ -35,12 +38,21 @@ UIImage::UIImage( Anchor a, math::vec2 extent, math::vec4 color, Texture t )
     assert( t );
 }
 
-UIImage::UIImage( Anchor a )
+Image::Image( Anchor a )
 : Widget{ a }
 {
 }
 
-void UIImage::render( RenderContext rctx ) const
+Image::Image( math::vec2 position, math::vec2 extent, DataModel* dataModel )
+: Widget{ position, extent }
+, m_dataModel{ dataModel }
+{
+    assert( dataModel );
+    m_current = m_dataModel->current();
+    setTexture( dataModel->texture( m_current ) );
+}
+
+void Image::render( RenderContext rctx ) const
 {
     assert( m_texture );
     const PushBuffer pushBuffer{
@@ -68,13 +80,28 @@ void UIImage::render( RenderContext rctx ) const
     rctx.renderer->push( pushBuffer, &pushConstant );
 }
 
-void UIImage::setColor( math::vec4 c )
+void Image::update( const UpdateContext& )
+{
+    if ( !m_dataModel ) {
+        return;
+    }
+    DataModel::size_type idx = m_dataModel->current();
+    if ( idx == m_current ) {
+        return;
+    }
+    m_current = idx;
+    setTexture( m_dataModel->texture( m_current ) );
+}
+
+void Image::setColor( math::vec4 c )
 {
     m_color = c;
 }
 
-void UIImage::setTexture( Texture t )
+void Image::setTexture( Texture t )
 {
     assert( t );
     m_texture = t;
+}
+
 }
--- src/ui_image.hpp
+++ src/ui_image.hpp
@@ -2,23 +2,34 @@
 
 #include "widget.hpp"
 #include "colors.hpp"
+#include "ui_data_model.hpp"
 
 #include <engine/math.hpp>
+#include <engine/update_context.hpp>
 #include <renderer/texture.hpp>
 
-class UIImage : public Widget {
+namespace ui {
+
+class Image : public Widget {
+    DataModel* m_dataModel = nullptr;
+    DataModel::size_type m_current = 0;
     math::vec4 m_color = color::white;
     Texture m_texture{};
 
 public:
-    UIImage() = default;
-    UIImage( Texture t );
-    UIImage( Texture t, math::vec4 color );
-    UIImage( math::vec2 position, math::vec2 extent, math::vec4 color, Texture );
-    UIImage( Anchor a, math::vec2 extent, math::vec4 color, Texture );
-    UIImage( Anchor );
+    Image() = default;
+    Image( Texture t );
+    Image( Texture t, math::vec4 color );
+    Image( math::vec2 position, math::vec2 extent, math::vec4 color, Texture );
+    Image( Anchor a, math::vec2 extent, math::vec4 color, Texture );
+    Image( Anchor );
+    Image( math::vec2 position, math::vec2 extent, DataModel* );
 
     virtual void render( RenderContext ) const override;
+    virtual void update( const UpdateContext& ) override;
     void setColor( math::vec4 );
     void setTexture( Texture );
 };
+}
+
+using UIImage = ui::Image;
