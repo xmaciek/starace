49cefecf8d5ad6f508714dface2e0bcc421c6c66
Maciej Latocha <latocha.maciek@gmail.com>
2022-12-10 10:02:21 +0100

    add mipmap generator

--- src/cooker_dds.cpp
+++ src/cooker_dds.cpp
@@ -7,9 +7,10 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <memory_resource>
+#include <string>
+#include <tuple>
+#include <vector>
 
 std::tuple<dds::dxgi::Format, tga::Header, std::pmr::vector<uint8_t>> convertTga( const std::filesystem::path& srcFile )
 {
@@ -49,7 +50,7 @@ std::tuple<dds::dxgi::Format, tga::Header, std::pmr::vector<uint8_t>> convertTga
         ifs.read( reinterpret_cast<char*>( indexes.data() ), static_cast<std::streamsize>( indexes.size() ) );
         ifs.close();
 
-        auto Unmap = []( const auto* colorMap, const auto& indexes, auto* dataOut )
+        auto unmap = []( const auto* colorMap, const auto& indexes, auto* dataOut )
         {
             auto convert = [colorMap]( auto idx )
             {
@@ -60,7 +61,7 @@ std::tuple<dds::dxgi::Format, tga::Header, std::pmr::vector<uint8_t>> convertTga
         switch ( header.colorMap[ 4 ] ) {
         case 8:
             ret.resize( header.width * header.height );
-            Unmap( reinterpret_cast<B*>( colorMap.data() ), indexes, reinterpret_cast<B*>( ret.data() ) );
+            unmap( reinterpret_cast<B*>( colorMap.data() ), indexes, reinterpret_cast<B*>( ret.data() ) );
             return { dds::dxgi::Format::R8_UNORM, header, ret };
 
         case 24: {
@@ -77,11 +78,11 @@ std::tuple<dds::dxgi::Format, tga::Header, std::pmr::vector<uint8_t>> convertTga
 
         case 32:
             ret.resize( sizeof( uint32_t ) * header.width * header.height );
-            Unmap( reinterpret_cast<BGRA*>( colorMap.data() ), indexes, reinterpret_cast<BGRA*>( ret.data() ) );
+            unmap( reinterpret_cast<BGRA*>( colorMap.data() ), indexes, reinterpret_cast<BGRA*>( ret.data() ) );
             return { dds::dxgi::Format::B8G8R8A8_UNORM, header, ret };
 
         default:
-            std::cout << "[ FAIL ] unhandled colormap bbp (" << (int)header.colorMap[ 4 ] << ") " << srcFile << std::endl;
+            std::cout << "[ FAIL ] unhandled colormap bpp (" << (int)header.colorMap[ 4 ] << ") " << srcFile << std::endl;
             return { {}, {}, {} };
         }
     }
@@ -124,10 +125,77 @@ std::tuple<dds::dxgi::Format, tga::Header, std::pmr::vector<uint8_t>> convertTga
     }
 }
 
+struct MipGen {
+    const uint32_t* pixels = nullptr;
+    uint32_t width = 0;
+    uint32_t idx = 0;
+    uint32_t operator () ()
+    {
+        auto indexes = []( uint32_t n, uint32_t width ) -> std::tuple<uint32_t, uint32_t, uint32_t, uint32_t>
+        {
+            n <<= 1u;
+            n += ( n / width ) * width;
+            return { n, n + 1u, n + width, n + width + 1u };
+        };
+
+        auto average = []( uint32_t a, uint32_t b, uint32_t c, uint32_t d ) -> uint32_t
+        {
+            auto sampler = []( uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t mask, uint32_t shift )
+            {
+                uint32_t ret = ( ( a & mask ) >> shift )
+                    + ( ( b & mask ) >> shift )
+                    + ( ( c & mask ) >> shift )
+                    + ( ( d & mask ) >> shift )
+                    ;
+                return ( ret >> 2u ) << shift;
+            };
+            return sampler( a, b, c, d, 0xFFu, 0u )
+                |  sampler( a, b, c, d, 0xFF00u, 8u )
+                |  sampler( a, b, c, d, 0xFF0000u, 16u )
+                |  sampler( a, b, c, d, 0xFF000000u, 24u )
+                ;
+        };
+
+        auto [ a, b, c, d ] = indexes( idx++, width );
+        return average( pixels[ a ], pixels[ b ], pixels[ c ], pixels[ d ] );
+    }
+
+    static std::pmr::vector<uint32_t> gib( uint32_t width, uint32_t height, const uint32_t* pixels )
+    {
+        assert( width );
+        assert( height );
+        assert( pixels );
+        std::pmr::vector<uint32_t> mip;
+        mip.resize( ( width * height ) >> 2u );
+        std::generate( mip.begin(), mip.end(), MipGen{ pixels, width } );
+        return mip;
+    }
+
+    static std::pmr::vector<std::pmr::vector<uint32_t>> genMips( uint32_t width, uint32_t height, const uint32_t* pixels )
+    {
+        assert( width );
+        assert( height );
+        assert( pixels );
+        uint32_t mipToGen = 0;
+        auto test = []( uint32_t d ) { return ( ( d % 4 ) == 0 ) && ( d > 4 ); };
+        while ( test( width >> mipToGen ) && test( height >> mipToGen ) ) {
+            mipToGen++;
+        }
+        std::pmr::vector<std::pmr::vector<uint32_t>> ret;
+        ret.resize( mipToGen );
+        for ( uint32_t i = 0; i < mipToGen; ++i ) {
+            ret[ i ] = gib( width >> i, height >> i, pixels );
+            pixels = ret[ i ].data();
+        }
+        return ret;
+    }
+};
+
+
 int main( int argc, char** argv )
 {
-    if ( argc != 3 ) {
-        std::cout << "[ FAIL ] invalid number of arguments" << std::endl;
+    if ( argc < 3 ) {
+        std::cout << "[ FAIL ] invalid number of arguments, expected >= 2, got " << argc - 1 << std::endl;
         return 1;
     }
 
@@ -144,15 +212,28 @@ int main( int argc, char** argv )
     }
 
     auto [ format, tgaHeader, pixels ] = convertTga( src );
+
     if ( format == dds::dxgi::Format::UNKNOWN ) { return 1; }
+
+    auto findParam = []( int argc, char** argv, const char* value )
+    {
+        char** end = argv + argc;
+        char** ret = std::find_if( argv, end, [value](const char* v) { return std::strcmp( v, value ) == 0; } );
+        return ret != end ? ret : nullptr;
+    };
+
+    std::pmr::vector<std::pmr::vector<uint32_t>> mips = findParam( argc - 3, argv + 3, "mipgen" )
+        ? MipGen::genMips( tgaHeader.width, tgaHeader.height, reinterpret_cast<const uint32_t*>( pixels.data() ) )
+        : std::pmr::vector<std::pmr::vector<uint32_t>>();
 
     dds::Header header{
         .flags = dds::constants::uncompressed,
         .height = tgaHeader.height,
         .width = tgaHeader.width,
         .pitchOrLinearSize = static_cast<uint32_t>( pixels.size() ),
-        .mipMapCount = 1,
+        .mipMapCount = static_cast<uint32_t>( mips.size() ) + 1u,
         .pixelFormat = dds::constants::DXGI,
+        .caps = static_cast<dds::Header::Caps>( dds::Header::Caps::fTexture | dds::Header::Caps::fMipMap ),
     };
 
     dds::dxgi::Header dxgiHeader{
@@ -168,5 +249,8 @@ int main( int argc, char** argv )
     ofs.write( reinterpret_cast<const char*>( &header ), static_cast<std::streamsize>( sizeof( header ) ) );
     ofs.write( reinterpret_cast<const char*>( &dxgiHeader ), static_cast<std::streamsize>( sizeof( dxgiHeader ) ) );
     ofs.write( reinterpret_cast<const char*>( pixels.data() ), static_cast<std::streamsize>( pixels.size() ) );
+    for ( const auto& mip : mips ) {
+        ofs.write( reinterpret_cast<const char*>( mip.data() ), static_cast<std::streamsize>( mip.size() * 4 ) );
+    }
     return 0;
 }
