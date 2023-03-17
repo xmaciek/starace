0067b616e3f378e0cae87495801f33fb4670dd0a
Maciej Latocha <latocha.maciek@gmail.com>
2023-03-17 22:28:23 +0100

    remove unused actions

--- src/game_action.hpp
+++ src/game_action.hpp
@@ -3,15 +3,7 @@
 #include <engine/action.hpp>
 
 enum class GameAction : Action::Enum {
-    eMenuConfirm,
-    eMenuCancel,
-    eMenuUp,
-    eMenuDown,
-    eMenuLeft,
-    eMenuRight,
-
     eGamePause,
-
     eJetPitch,
     eJetYaw,
     eJetRoll,
