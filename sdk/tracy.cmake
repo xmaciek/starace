eb63e68f97a706aeba45e33c8821128807c1acd0
Maciej Latocha <latocha.maciek@gmail.com>
2024-11-30 17:05:24 +0100

    change tracy options

--- sdk/tracy.cmake
+++ sdk/tracy.cmake
@@ -1,8 +1,8 @@
 option( ENABLE_TRACY_PROFILER "Whether to enable tracy profiler" FALSE )
 
 if ( ${ENABLE_TRACY_PROFILER} )
-    set( TRACY_CALLSTACK ON )
-    set( TRACY_ENABLE ON )
+    set( TRACY_CALLSTACK ON CACHE BOOL "" )
+    set( TRACY_ENABLE ON CACHE BOOL "" )
     add_subdirectory( tracy )
     set_vs_directory( TracyClient "sdk" )
 endif()
